463732a8b7097f6a96f69a21c07cd58c2e5631ea
Hyeon Ho <hhwj2270@gmail.com>
2025-10-13 22:51:04 +0900

    [level 2] Title: 기능개발, Time: 0.33 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,55 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-
-        int n = progresses.length;
-        
-        Deque<Integer> feature = new ArrayDeque<>();
-        
-        for (int i = 0; i < n; i ++) {
-            feature.push(progresses[i]);
-        }
-        
-        int finishCount = 0;
-        
-        int checkIdx = 0;
-        
-        List<Integer> finishedList = new ArrayList<>();
-        
-        while(!feature.isEmpty()) {
-            
-            // 끝났으면 pop하고 finishCount +1 몇 개 동시에 끝냈는지
-            if (progresses[checkIdx] >= 100) {
-                finishCount ++;
-                checkIdx ++;
-                feature.pop();
-                if (feature.isEmpty()) {
-                    finishedList.add(finishCount);
-                }
-                
-            } else {
-            // 끝나기 않을 때 finishCount를 finishedList 배열에 넣어주고 finishCount = 0
-
-                if (finishCount > 0) {
-                    finishedList.add(finishCount);
-                    finishCount = 0;
-                }
-                // 진행도 계산
-                for (int i = checkIdx; i < n; i ++) {
-                    progresses[i] += speeds[i];
-                }
-                
-            }
-        }
-        
-        int totalRelease = finishedList.size();
-        
-        int[] answer = new int[totalRelease];
-        for (int i = 0; i < totalRelease; i++) {
-            answer[i] = finishedList.get(i);
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
