702dc39e0c1c2cd78c8ec6eeb622ea20a7fbe6c2
Hyeon Ho <hhwj2270@gmail.com>
2025-10-16 11:19:01 +0900

    [level 2] Title: 주식가격, Time: 25.53 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,25 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
-        int n = prices.length;
-        int[] answer = new int[n];
-
-        ArrayDeque<Integer> priceDeque = new ArrayDeque<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!priceDeque.isEmpty() && prices[i] < prices[priceDeque.peekLast()]) {
-                int cur = priceDeque.removeLast();
-                answer[cur] = i - cur;
-            }
-            priceDeque.addLast(i);
-        }
-
-        while (!priceDeque.isEmpty()) {
-            int cur = priceDeque.removeLast();
-            answer[cur] = (n - 1) - cur;
-        }
-
+        int[] answer = {};
         return answer;
     }
 }
