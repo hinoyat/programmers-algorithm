83b87274d31779973ab622cad322a051d6194933
Hyeon Ho <hhwj2270@gmail.com>
2025-12-09 23:43:07 +0900

    [level 2] Title: 피보나치 수, Time: 3.98 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    static final int MOD = 1234567;
+
+    public int solution(int n) {
+        int answer = 0;
+
+        int[] dp = new int[n + 1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        answer = dp[n];
+
+        return answer;
+    }
+}
