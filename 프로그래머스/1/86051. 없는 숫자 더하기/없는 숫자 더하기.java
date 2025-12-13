ca8f6947cb05adaeced80b1f0079f5771766c689
Hyeon Ho <hhwj2270@gmail.com>
2025-12-13 22:39:37 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int sum = 45;
+        
+        for (int n : numbers) {
+            sum -= n;
+        }
+        
+        return sum;
+    }
+}
